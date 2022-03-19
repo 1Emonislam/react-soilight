a2388345709079d23ea5b3e95cfb0bf5b425555a
1emonislam <emonislamweb@gmail.com>
2022-03-19 11:10:53 +0600

    product cancell

--- src/pages/Dashboard/Sheard/SearchProfileView.js
+++ src/pages/Dashboard/Sheard/SearchProfileView.js
@@ -81,14 +81,18 @@ function SearchProfileView({ data = {}, handleApproved, withdrawCancelled, withd
                                 </div>
                             </Grid>
                             <Grid item xs={4}>
-                                <div className="item-view" style={{ borderLeft: '0px', padding: '0px 10px', marginTop: '30px', marginBottom: '80px' }}>
+                                <div className="item-view" style={{ borderLeft: '0px', padding: '0px 10px', marginTop: '30px',marginBottom:'0px' }}>
                                     <Button variant="contained" onClick={() => productApproved(data?._id)} style={{ textTransform: 'capitalize', background: '#05AC54', padding: '10px 30px', borderRadius: '8px' }}>Product Approved </Button>
                                 </div>
                             </Grid>
                             <Grid item xs={4}>
-                                <div className="item-view" style={{ borderLeft: '0px', padding: '0px 10px', marginTop: '30px', marginBottom: '80px' }}>
+                                <div className="item-view" style={{ borderLeft: '0px', padding: '0px 10px', marginTop: '30px',marginBottom:'0px' }}>
                                     <Button variant="contained" onClick={() => productCancelled(data?._id)} style={{ textTransform: 'capitalize', background: 'red', padding: '10px 30px', borderRadius: '8px' }}>Product Cancelled </Button>
                                 </div>
+                            </Grid>
+                            <Grid item xs={10} md={6} style={{ marginBottom: '50px', marginTop: '20px', marginLeft: '85px' }}>
+                                {success && <Alert severity="success" timeout={5000} md={6}>{success}</Alert>}
+                                {error && <Alert severity="error" timeout={5000} md={6}>{error}</Alert>}
                             </Grid>
                         </>
                     }
@@ -177,14 +181,18 @@ function SearchProfileView({ data = {}, handleApproved, withdrawCancelled, withd
                                 </Grid>
                             </Grid>
                             <Grid item xs={4}>
-                                <div className="item-view" style={{ borderLeft: '0px', padding: '0px 10px', marginTop: '30px', marginBottom: '80px' }}>
+                                <div className="item-view" style={{ borderLeft: '0px', padding: '0px 10px', marginTop: '30px',marginBottom:'0px' }}>
                                     <Button variant="contained" disabled={isOpen} onClick={() => withdrawApproved(data?._id)} style={{ textTransform: 'capitalize', background: '#05AC54', padding: '10px 30px', borderRadius: '8px' }}>Withdaraw Approved </Button>
                                 </div>
                             </Grid>
                             <Grid item xs={4}>
-                                <div className="item-view" style={{ borderLeft: '0px', padding: '0px 10px', marginTop: '30px', marginBottom: '80px' }}>
+                                <div className="item-view" style={{ borderLeft: '0px', padding: '0px 10px', marginTop: '30px',marginBottom:'0px' }}>
                                     <Button variant="contained" disabled={isOpen} onClick={() => withdrawCancelled(data?._id)} style={{ textTransform: 'capitalize', background: 'red', padding: '10px 30px', borderRadius: '8px' }}>Withdraw Cancelled </Button>
                                 </div>
+                            </Grid>
+                            <Grid item xs={10} md={6} style={{ marginBottom: '50px', marginTop: '20px', marginLeft: '85px' }}>
+                                {success && <Alert severity="success" timeout={5000} md={6}>{success}</Alert>}
+                                {error && <Alert severity="error" timeout={5000} md={6}>{error}</Alert>}
                             </Grid>
                         </>
                     }
@@ -266,12 +274,12 @@ function SearchProfileView({ data = {}, handleApproved, withdrawCancelled, withd
                             <Grid item xs={12} md={6}>
                             </Grid>
                             <Grid item xs={4}>
-                                <div className="item-view" style={{ borderLeft: '0px', padding: '0px 10px', marginTop: '30px', marginBottom: '80px' }}>
+                                <div className="item-view" style={{ borderLeft: '0px', padding: '0px 10px', marginTop: '30px',marginBottom:'0px' }}>
                                     <Button variant="contained" onClick={() => riderApproved(data?._id)} style={{ textTransform: 'capitalize', background: '#05AC54', padding: '10px 50px', borderRadius: '8px' }}>Approved </Button>
                                 </div>
                             </Grid>
                             <Grid item xs={4}>
-                                <div className="item-view" style={{ borderLeft: '0px', padding: '0px 10px', marginTop: '30px', marginBottom: '80px' }}>
+                                <div className="item-view" style={{ borderLeft: '0px', padding: '0px 10px', marginTop: '30px',marginBottom:'0px' }}>
                                     <Button variant="contained" onClick={() => riderCancelled(data?._id)} style={{ textTransform: 'capitalize', background: 'red', padding: '10px 50px', borderRadius: '8px' }}>Cancelled </Button>
                                 </div>
                             </Grid>
@@ -390,16 +398,19 @@ function SearchProfileView({ data = {}, handleApproved, withdrawCancelled, withd
                             {seller && <Grid item xs={12} md={6} style={{}}>
                             </Grid>}
                             <Grid item xs={4}>
-                                <div className="item-view" style={{ borderLeft: '0px', padding: '0px 10px', marginTop: '30px', marginBottom: '80px' }}>
+                                <div className="item-view" style={{ borderLeft: '0px', padding: '0px 10px', marginTop: '30px',marginBottom:'0px' }}>
                                     <Button variant="contained" onClick={() => handleApproved(data?._id)} style={{ textTransform: 'capitalize', background: '#05AC54', padding: '10px 50px', borderRadius: '8px' }}>Approved </Button>
                                 </div>
                             </Grid>
                             <Grid item xs={4}>
-                                <div className="item-view" style={{ borderLeft: '0px', padding: '0px 10px', marginTop: '30px', marginBottom: '80px' }}>
+                                <div className="item-view" style={{ borderLeft: '0px', padding: '0px 10px', marginTop: '30px',marginBottom:'0px' }}>
                                     <Button variant="contained" onClick={() => handleRejected(data?._id)} style={{ textTransform: 'capitalize', background: 'red', padding: '10px 50px', borderRadius: '8px' }}>Rejected </Button>
                                 </div>
                             </Grid>
-
+                            <Grid item xs={10} md={6} style={{ marginBottom: '50px', marginTop: '20px', marginLeft: '85px' }}>
+                                {success && <Alert severity="success" timeout={5000} md={6}>{success}</Alert>}
+                                {error && <Alert severity="error" timeout={5000} md={6}>{error}</Alert>}
+                            </Grid>
                         </>
                         }
                         {order && <>
@@ -536,12 +547,12 @@ function SearchProfileView({ data = {}, handleApproved, withdrawCancelled, withd
                                         <span style={{ color: 'black', fontWeight: '500' }}> Total: ${data?.products?.reduce((perv, curr) => (perv + Number(curr?.price)), 0)}</span>
                                     </Grid>
                                     <Grid item xs={4}>
-                                        <div className="item-view" style={{ borderLeft: '0px', padding: '0px 10px', marginTop: '30px', marginBottom: '80px' }}>
+                                        <div className="item-view" style={{ borderLeft: '0px', padding: '0px 10px', marginTop: '30px',marginBottom:'0px' }}>
                                             <Button variant="contained" disabled={isOpen} onClick={() => orderDelivered(data?._id)} style={{ textTransform: 'capitalize', background: '#05AC54', padding: '10px 45px', borderRadius: '8px' }}> Order Delivered </Button>
                                         </div>
                                     </Grid>
                                     <Grid item xs={4}>
-                                        <div className="item-view" style={{ borderLeft: '0px', padding: '0px 10px', marginTop: '30px', marginBottom: '80px' }}>
+                                        <div className="item-view" style={{ borderLeft: '0px', padding: '0px 10px', marginTop: '30px',marginBottom:'0px' }}>
                                             <Button disabled={isOpen} variant="contained" onClick={() => orderCancelled(data?._id)} style={{ textTransform: 'capitalize', background: 'red', padding: '10px 50px', borderRadius: '8px' }}>Order Cancel</Button>
                                         </div>
                                     </Grid>
